1b82bd9430f326cab038ce4d31f2ec15cad181e1
Révész Timóteus (SDA) <ReveszT@rcinet.local>
2020-02-05 16:30:06 +0100

    Token Authentication

---
+++ datingapp.api/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace datingapp.api.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength = 8, ErrorMessage="Pasword should be at least 8 characters long")]
+        public string Password { get; set; }
+    }
+}
